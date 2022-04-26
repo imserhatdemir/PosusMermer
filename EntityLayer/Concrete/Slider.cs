b403cf916e1c31e7ea50b43f74362f6e60d171d0
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2022-04-27 02:49:12 +0300

    update2

---
+++ EntityLayer/Concrete/Slider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Slider
+    {
+        [Key]
+        public int SliderID { get; set; }
+        [StringLength(100)]
+        public string Title1 { get; set; }
+        [StringLength(100)]
+        public string Description1 { get; set; }
+        [StringLength(250)]
+        public string Image1 { get; set; }
+        [StringLength(100)]
+        public string Title2 { get; set; }
+        [StringLength(100)]
+        public string Description2 { get; set; }
+        [StringLength(300)]
+        public string Image2 { get; set; }
+        [StringLength(100)]
+        public string Title3 { get; set; }
+        [StringLength(100)]
+        public string Description3 { get; set; }
+        [StringLength(300)]
+        public string Image3 { get; set; }
+    }
+}
---
+++ posusmermer/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace posusmermer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult CategoryList()
+        {
+            var values = cm.GetAll();
+            return PartialView(values);
+        }
+        
+    }
+}
